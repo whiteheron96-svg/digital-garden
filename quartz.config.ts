81893c6d29e2cbd8c5ef385639c48f5d2c4bdfb4
whiteheron96-svg <whiteheron96@gmail.com>
2026-06-04 12:12:16 +0900

    Add google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-RBJMQOSQ1"
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
