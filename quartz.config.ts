9f524162a1b301c444f50c6c315d40cca543e657
whiteheron96-svg <whiteheron96@gmail.com>
2026-06-04 12:14:32 +0900

    Add baseURL or github pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
       tagId: "G-RBJMQOSQ1"
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://whiteheron96-svg.github.io/digital-garden",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
